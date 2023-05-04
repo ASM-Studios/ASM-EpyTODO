9c0065e6c7d3a84f1d368dc7892e36fee4116bcf
Mael RABOT <mael.rabot@epitech.eu>
2023-05-04 14:53:25 +0200

    Update index.js
    Signed-off-by: Mael RABOT <mael.rabot@epitech.eu>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const { text } = require("express")
-
 const app = express()
 const port = process.env.PORT
-
-
-const bcrypt = require('bcryptjs')
-
 const db = require('./config/db')
 
 const auth = require('./middleware/auth')
-
 const { userRoute } = require('./routes/user/user')
 
 app.use(express.urlencoded({ extended: true }))
@@ -21,39 +15,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-const password = 'pass123';
-
-// Encryption of the string password
-bcrypt.genSalt(10, function (err, Salt) {
-
-    // The bcrypt is used for encrypting password.
-    bcrypt.hash(password, Salt, function (err, hash) {
-
-        if (err) {
-            return console.log('Cannot encrypt');
-        }
-
-        const hashedPassword = hash;
-        console.log(hash);
-
-        bcrypt.compare(password, hashedPassword,
-            async function (err, isMatch) {
-
-                // Comparing the original password to
-                // encrypted password
-                if (isMatch) {
-                    console.log('Encrypted password is: ', password);
-                    console.log('Decrypted password is: ', hashedPassword);
-                }
-
-                if (!isMatch) {
-
-                    // If password doesn't match the following
-                    // message will be sent
-                    console.log(hashedPassword + ' is not encryption of '
-                        + password);
-                }
-            })
+app.get('/get', (req, res) => {
+    db.query('SELECT * FROM `user`', function (err, result) {
+        res.send(result)
     })
 })
 
